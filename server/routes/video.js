33b46b04da9c8fe11367afa8f0001451f67027ef
bbaing4564 <bbaing4564@gmail.com>
2020-07-18 19:44:41 +0900

    Add Landing page

--- server/routes/video.js
+++ server/routes/video.js
@@ -45,20 +45,29 @@ router.post("/uploadfiles", (req, res) => {
 
 router.post("/uploadVideo", (req, res) => {
   // 비디오 정보를 db에 저장
-  const video = new Video(req.body)
+  const video = new Video(req.body);
 
   video.save((err, video) => {
-      if(err) return res.status(400).json({ success: false, err })
-      return res.status(200).json({
-          success: true 
-      })
-  })
+    if (err) return res.status(400).json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
+
+router.get("/getvideos", (req, res) => {
+  // 비디오를 db에서 가져와 클라이언트에 보냄
+  Video.find()
+    .populate('writer')
+    .exec((err, videos) => {
+      if(err) return res.status(400).send(err);
+      res.status(200).json({ success: true, videos })
+    })
 
 });
 
 router.post("/thumbnail", (req, res) => {
   // 썸네일 생성 & 비디오 러닝타임 가져오기
-
   let filePath = "";
   let fileDuration = "";
 
